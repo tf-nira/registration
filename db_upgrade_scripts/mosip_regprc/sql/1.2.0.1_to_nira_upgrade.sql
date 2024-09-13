ab58fbbedcc9f658da4113579af9b016bf57b1c3
Sowmya U B <sowmya.ub@technoforte.co.in>
2024-09-13 13:40:37 +0530

    scripts updated
    Signed-off-by: Sowmya U B <sowmya.ub@technoforte.co.in>

---
+++ db_upgrade_scripts/mosip_regprc/sql/1.2.0.1_to_nira_upgrade.sql
@@ -0,0 +1,27 @@
+\c mosip_regprc
+
+REASSIGN OWNED BY sysadmin TO postgres;
+
+REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA regprc FROM regprcuser;
+
+REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA regprc FROM sysadmin;
+
+GRANT SELECT, INSERT, TRUNCATE, REFERENCES, UPDATE, DELETE ON ALL TABLES IN SCHEMA regprc TO regprcuser;
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA regprc TO postgres;
+
+----------------------------------------------Multiple table level changes on regprc db-------------------------------------------------------
+CREATE TABLE consumed__nin_introducer (
+    nin character varying (500) PRIMARY KEY,
+    usage_count INTEGER,
+    last_used TIMESTAMP WITHOUT TIME ZONE
+);
+
+ALTER TABLE regprc.individual_demographic_dedup ADD COLUMN nrcid character varying(64) NOT NULL;
+
+INSERT INTO regprc.transaction_type (code,descr,lang_code,is_active,cr_by,cr_dtimes,upd_by,upd_dtimes,is_deleted,del_dtimes) VALUES
+	 ('CITIZENSHIP_VERIFICATION','transaction_done','eng',true,'MOSIP_SYSTEM',now(),NULL,NULL,false,NULL);
+INSERT INTO regprc.transaction_type (code,descr,lang_code,is_active,cr_by,cr_dtimes,upd_by,upd_dtimes,is_deleted,del_dtimes) VALUES
+	 ('PAYMENT_VALIDATION','transaction_done','eng',true,'MOSIP_SYSTEM',now(),NULL,NULL,false,NULL);
+
+
