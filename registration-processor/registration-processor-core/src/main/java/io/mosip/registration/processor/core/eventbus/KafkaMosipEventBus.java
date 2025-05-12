f3c65e6f77ce219ab2440939f06e39cb52f08cb9
nagarjunakuchi <nagarjunabtechece@gmail.com>
2025-05-12 05:48:24 +0530

    added timeouts for kafka

--- registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/eventbus/KafkaMosipEventBus.java
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/eventbus/KafkaMosipEventBus.java
@@ -98,6 +98,8 @@ public class KafkaMosipEventBus implements MosipEventBus {
 		consumerConfig.put("group.id", groupId);
 		consumerConfig.put("auto.offset.reset", "latest");
 		consumerConfig.put("max.poll.records", maxPollRecords);
+		consumerConfig.put(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, "500000");
+		consumerConfig.put(ConsumerConfig.REQUEST_TIMEOUT_MS_CONFIG, "560000");
 		consumerConfig.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "540000");
 		consumerConfig.put(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG,"180000");
 		if (commitType.equals("auto"))
