e147e3cfc334283bb738f4fd12907b5a4ac7ac71
Manishch22 <126442522+Manishch22@users.noreply.github.com>
2025-06-30 12:47:48 +0530

    updated country code

--- registration-processor/registration-processor-notification-service/src/main/java/io/mosip/registration/processor/notification/service/impl/NotificationServiceImpl.java
+++ registration-processor/registration-processor-notification-service/src/main/java/io/mosip/registration/processor/notification/service/impl/NotificationServiceImpl.java
@@ -367,7 +367,7 @@ public class NotificationServiceImpl implements NotificationService {
 						countryCode = countryCodeArray.getJSONObject(0).getString("value");
 					}
 					
-					if (countryCode != null && countryCodeVal.equals("UGANDA (+256)")) {
+					if (countryCode != null && countryCodeVal.equals("Uganda (256)")) {
 						isSMSSuccess = sendSms(id, process, attributes, regType, messageSenderDto, description);
 					}
 				} else if (notificationType.equalsIgnoreCase(NotificationTypeEnum.EMAIL.name())
