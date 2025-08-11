213816085f8f3642ab640e7c8e2ada0c70b99dec
Nagarjuna K <64436505+Nagarjunakuchi88@users.noreply.github.com>
2025-08-11 23:26:21 +0530

    Update RegistrationRepositary.java

--- registration-processor/registration-processor-registration-status-service-impl/src/main/java/io/mosip/registration/processor/status/repositary/RegistrationRepositary.java
+++ registration-processor/registration-processor-registration-status-service-impl/src/main/java/io/mosip/registration/processor/status/repositary/RegistrationRepositary.java
@@ -60,7 +60,7 @@ public interface RegistrationRepositary<T extends BaseRegistrationEntity, E> ext
 	@Query(value ="SELECT * FROM registration r WHERE r.status_code IN :statusCodes AND r.resume_timestamp < now() AND r.default_resume_action is NOT NULL order by r.upd_dtimes LIMIT :fetchSize ", nativeQuery = true)
 	public List<RegistrationStatusEntity> getActionablePausedPackets(@Param("statusCodes") List<String> statusCodes,@Param("fetchSize") Integer fetchSize);
 
-	@Query(value = "SELECT * FROM registration r WHERE r.status_code =:statusCode AND r.reg_stage_name NOT IN :excludeStageNames order by r.upd_dtimes LIMIT :fetchSize ", nativeQuery = true)
+	@Query(value = "SELECT * FROM registration r WHERE r.status_code =:statusCode AND r.reg_stage_name NOT IN :excludeStageNames order by r.upd_dtimes desc LIMIT :fetchSize ", nativeQuery = true)
 	public List<RegistrationStatusEntity> getResumablePackets(@Param("statusCode") String statusCode,
 			@Param("fetchSize") Integer fetchSize, @Param("excludeStageNames") List<String> excludeStageNames);
 	
@@ -71,4 +71,5 @@ public interface RegistrationRepositary<T extends BaseRegistrationEntity, E> ext
 	public List<RegistrationStatusEntity> getAnonymousNotAddedPackets(@Param("fetchSize") Integer fetchSize);
 	
 }
+
 
