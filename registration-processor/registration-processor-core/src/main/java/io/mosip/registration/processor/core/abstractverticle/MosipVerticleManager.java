77ea23c88e1e0de186c40b8d6b36e5fc1ebc09ec
Manishch22 <126442522+Manishch22@users.noreply.github.com>
2025-08-13 22:44:45 +0530

    Moved the send code after thread success

--- registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/abstractverticle/MosipVerticleManager.java
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/abstractverticle/MosipVerticleManager.java
@@ -244,14 +244,15 @@ public abstract class MosipVerticleManager extends AbstractVerticle
 					addTagsToMessageDTO(message);
 				}
 				message.setLastHopTimestamp(DateUtils.formatToISOString(DateUtils.getUTCCurrentDateTime()));
-				mosipEventBus.send(toAddress, message);
 				promise.complete();
 			} catch (Exception e) {
 				promise.fail(e);
 			}
 		}, res -> {
 			if (res.succeeded()) {
-				logger.info("Vertex thread sucess: message sent");
+				logger.info("Vertex thread sucess: kafka send initiated");
+				mosipEventBus.send(toAddress, message);
+				logger.info("Vertex thread sucess: kafka send completed");
 			} else {
 				logger.error("Vertex thread failed: " + res.cause());
 			}
