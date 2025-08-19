be851eb19e4b03c0bda170031d95700f1741c617
jagadeesh.t <jagadeesh.t@TECHNO-566.technoforte.co.in>
2025-08-19 12:42:44 +0530

    Updated

--- registration-processor/registration-processor-registration-status-service-impl/src/main/java/io/mosip/registration/processor/status/dao/RegistrationStatusDao.java
+++ registration-processor/registration-processor-registration-status-service-impl/src/main/java/io/mosip/registration/processor/status/dao/RegistrationStatusDao.java
@@ -124,8 +124,8 @@ public class RegistrationStatusDao {
 			registrationStatusEntityList=registrationStatusRepositary.findByWorkflowInstanceId(workflowInstanceId);
 		} else if (process != null && Services.contains(process)) {
 			registrationStatusEntityList=registrationStatusRepositary.findByProcessANDRegId(rid, process);
-//		}else {
-//			registrationStatusEntityList=registrationStatusRepositary.findByRegId(rid);
+		}else {
+			registrationStatusEntityList=registrationStatusRepositary.findByRegId(rid);
 		}
 		return !registrationStatusEntityList.isEmpty() ? registrationStatusEntityList.get(0) : null;
 	}
