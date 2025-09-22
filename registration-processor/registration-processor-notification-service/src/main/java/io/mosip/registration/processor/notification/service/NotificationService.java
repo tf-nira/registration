3aab265c70a264df43a97423c1806f6774cb18f8
Soumya Raj <soumya.raj@technoforte.co.in>
2025-09-22 18:15:27 +0530

    modified notification scheduler for REJECTED & FAILED packets

--- registration-processor/registration-processor-notification-service/src/main/java/io/mosip/registration/processor/notification/service/NotificationService.java
+++ registration-processor/registration-processor-notification-service/src/main/java/io/mosip/registration/processor/notification/service/NotificationService.java
@@ -12,4 +12,5 @@ public interface NotificationService {
 
 	ResponseEntity<Void> process(WorkflowPausedForAdditionalInfoEventDTO object);
 
+	boolean sendNotificationProcess(WorkflowCompletedEventDTO object);
 }
