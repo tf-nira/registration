a080e9cec0b33b1ea7025fdb38636bb4d79316e9
Sowmya U B <sowmya.ub@technoforte.co.in>
2025-10-06 17:31:25 +0530

    performance fix

--- registration-processor/registration-processor-info-storage-service/src/main/java/io/mosip/registration/processor/packet/storage/utils/Utilities.java
+++ registration-processor/registration-processor-info-storage-service/src/main/java/io/mosip/registration/processor/packet/storage/utils/Utilities.java
@@ -634,17 +634,14 @@ public class Utilities {
 
 			IdResponseDTO1 idResponseDto;
 
-			String typeParam = "type";
-			String typeParamValue = "all";
+
 			String typeIdParam = "idType";
 			String typeIdParamValue = "handle";
 
 			List<String> queryParams = new ArrayList<>();
-			queryParams.add(typeParam);
 			queryParams.add(typeIdParam);
 
 			List<Object> queryParamValues = new ArrayList<Object>();
-			queryParamValues.add(typeParamValue);
 			queryParamValues.add(typeIdParamValue);
 
 
@@ -938,7 +935,7 @@ public class Utilities {
 			throws ApisResourceAccessException, IdRepoAppException, IOException {
 
 		if (nin != null) {
-			ResponseDTO idResponseDto = retrieveIdrepoResponseObjWithNIN(nin);
+			ResponseDTO idResponseDto = retrieveIdrepoResponseObjWithNIN(nin, false);
 			if (idResponseDto != null) {
 				String response = objMapper.writeValueAsString(idResponseDto.getIdentity());
 				regProcLogger.debug(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.UIN.toString(), "",
@@ -970,7 +967,7 @@ public class Utilities {
 	 * @return
 	 * @throws ApisResourceAccessException
 	 */
-	private ResponseDTO retrieveIdrepoResponseObjWithNIN(String nin)
+	private ResponseDTO retrieveIdrepoResponseObjWithNIN(String nin, boolean isBioNeeded)
 			throws ApisResourceAccessException {
 		if (nin != null) {
 			regProcLogger.debug(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.UIN.toString(), "",
@@ -985,11 +982,16 @@ public class Utilities {
 			String typeIdParamValue = "handle";
 			
 			List<String> queryParams = new ArrayList<>();
-			queryParams.add(typeParam);
+			if (isBioNeeded) {
+				queryParams.add(typeParam);
+			}
+
 			queryParams.add(typeIdParam);
 			
 			List<Object> queryParamValues = new ArrayList<Object>();
-			queryParamValues.add(typeParamValue);
+			if (isBioNeeded) {
+				queryParamValues.add(typeParamValue);
+			}
 			queryParamValues.add(typeIdParamValue);
 			
 
@@ -1070,7 +1072,7 @@ public class Utilities {
 		String handle = packetManagerService.getFieldByMappingJsonKey(id, MappingJsonConstants.NIN, process, stageName);
 		regProcLogger.debug(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.REGISTRATIONID.toString(), id,
 				"Utilities::getIdrepoResponseByHandle()::handleRetrieved");
-		ResponseDTO responseDTO = retrieveIdrepoResponseObjWithNIN(handle);
+		ResponseDTO responseDTO = retrieveIdrepoResponseObjWithNIN(handle, false);
 		return responseDTO;
 	}
 
