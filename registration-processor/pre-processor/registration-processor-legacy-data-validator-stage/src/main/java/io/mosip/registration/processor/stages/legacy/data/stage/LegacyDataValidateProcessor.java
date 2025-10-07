e73d3abbc8a3fcc2d59a4697cd53cee877d47c41
Soumya Raj <soumya.raj@technoforte.co.in>
2025-10-07 17:38:51 +0530

    minor notification message fix

--- registration-processor/pre-processor/registration-processor-legacy-data-validator-stage/src/main/java/io/mosip/registration/processor/stages/legacy/data/stage/LegacyDataValidateProcessor.java
+++ registration-processor/pre-processor/registration-processor-legacy-data-validator-stage/src/main/java/io/mosip/registration/processor/stages/legacy/data/stage/LegacyDataValidateProcessor.java
@@ -108,7 +108,7 @@ public class LegacyDataValidateProcessor {
 			updateDTOsAndLogError(registrationStatusDto, RegistrationStatusCode.PROCESSING,
 					StatusUtil.DATA_MIGRATION_API_FAILED, RegistrationExceptionTypeCode.DATA_MIGRATION_EXCEPTION,
 					description, PlatformErrorMessages.RPR_LEGACY_DATA_MIGRATION_API_FAILED, e);
-			attributes.put("FAILURE_REASON", "Invalid NIN");
+			attributes.put("FAILURE_REASON", "Potential mistype of NIN");
 			object.setNotificationAttributes(attributes);
 		} catch (LegacyDataValidationException e) {
 			updateDTOsAndLogError(registrationStatusDto, RegistrationStatusCode.LEGACYERROR,
