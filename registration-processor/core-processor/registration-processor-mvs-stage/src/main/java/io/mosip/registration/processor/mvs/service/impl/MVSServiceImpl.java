24233c1eba082e6dac5af2f06bc754ceb2deabaf
jagadeesh.t <jagadeesh.t@TECHNO-566.technoforte.co.in>
2025-10-08 12:48:47 +0530

    Updated

--- registration-processor/core-processor/registration-processor-mvs-stage/src/main/java/io/mosip/registration/processor/mvs/service/impl/MVSServiceImpl.java
+++ registration-processor/core-processor/registration-processor-mvs-stage/src/main/java/io/mosip/registration/processor/mvs/service/impl/MVSServiceImpl.java
@@ -822,7 +822,7 @@ public class MVSServiceImpl implements MVSService {
 			String lostPacketNin = applicantFields.get(MappingJsonConstants.NIN);
 			
 			JSONObject jsonObject = utility.getIdentityJSONObjectByHandle(lostPacketNin);
-			JSONArray districtArray = (JSONArray) jsonObject.get("applicantPlaceOfResidenceDistrict");
+			JSONArray districtArray = (JSONArray) jsonObject.get(MappingJsonConstants.DISTRICT);
 			if (districtArray != null) {
 			    JSONObject districtObject = (JSONObject) districtArray.get(0);
 			    String districtValue = (String) districtObject.get("value");
