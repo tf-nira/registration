91978b92e52d8ffc5cbb7031f76f8fa0990f570b
Soumya Raj <soumya.raj@technoforte.co.in>
2025-10-25 01:39:54 +0530

    query fix

--- registration-processor/registration-processor-info-storage-service/src/main/java/io/mosip/registration/processor/packet/storage/repository/BasePacketRepository.java
+++ registration-processor/registration-processor-info-storage-service/src/main/java/io/mosip/registration/processor/packet/storage/repository/BasePacketRepository.java
@@ -490,6 +490,6 @@ public interface BasePacketRepository<E extends BasePacketEntity<?>, T> extends 
 	@Query(value = "SELECT t FROM TransactionTypeEntity t WHERE t.id.code =:code")
 	public List<TransactionTypeEntity> getTransactionTypeByCode(@Param("code") String code);
 
-	@Query(value = "SELECT e FROM EnrollmentDataEntity e WHERE e.reg_id =:reg_id")
+	@Query(value = "SELECT e FROM EnrollmentDataEntity e WHERE e.id.reg_id =:reg_id")
 	public List<EnrollmentDataEntity> getEnrollmentData(@Param("reg_id") String regId);
 }
