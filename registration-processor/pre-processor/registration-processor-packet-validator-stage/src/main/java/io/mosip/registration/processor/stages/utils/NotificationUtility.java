2b1c5be2de961c6e2bdc15be738d02b161272411
jagadeesh.t <jagadeesh.t@TECHNO-566.technoforte.co.in>
2025-12-08 15:49:55 +0530

    Updated

--- registration-processor/pre-processor/registration-processor-packet-validator-stage/src/main/java/io/mosip/registration/processor/stages/utils/NotificationUtility.java
+++ registration-processor/pre-processor/registration-processor-packet-validator-stage/src/main/java/io/mosip/registration/processor/stages/utils/NotificationUtility.java
@@ -176,14 +176,26 @@ public class NotificationUtility {
                 MappingJsonConstants.VALUE);
 		String[] nameArray = nameField.toString().split(",");
 
+		String jsonServiceTypeObj =  packetManagerService.getField(regEntity.getRegistrationId(), MappingJsonConstants.SERVICE_TYPE, regEntity.getRegistrationType(), ProviderStageName.PACKET_VALIDATOR);
+		
+		String userServiceType = null;
+		if (jsonServiceTypeObj != null) {
+			JSONArray userServiceTypeArray = new JSONArray(jsonServiceTypeObj);
+			userServiceType = userServiceTypeArray.getJSONObject(0).getString("value");
+		}
+
 		for(String preferredLanguage:preferredLanguages) {
 			attributes.put(nameArray[0] + "_" + preferredLanguage, "applicant");
 
 			String userService = "";
-			if ("NEW".equals(regType)) {
+			if("Alien New Registration".equalsIgnoreCase(userServiceType)) {
+				userService = userServiceType;
+			} else if ("Renewal of Alien".equalsIgnoreCase(userServiceType)) {
+				userService = userServiceType;
+			} else if ("Alien Replacement".equalsIgnoreCase(userServiceType)) {
+				userService = userServiceType;
+			} else if ("NEW".equals(regType)) {
 				userService = "New Registration";
-			} else if ("ALIENNEW".equals(regType)) {
-				userService = "Alien New Registration";
 			} else if ("LOST".equals(regType)) {
 				userService = "Replacement Of Card";
 			} else if ("UPDATE".equals(regType)) {
