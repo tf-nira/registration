a3fb5a31186b86c020dc3d2a1941f7fd8779c99b
Soumya Raj <soumya.raj@technoforte.co.in>
2025-12-22 22:44:51 +0530

    reject packet in LegacyDataValidateProcessor for invalid nin

--- registration-processor/pre-processor/registration-processor-legacy-data-validator-stage/src/main/java/io/mosip/registration/processor/stages/legacy/data/stage/LegacyDataValidateProcessor.java
+++ registration-processor/pre-processor/registration-processor-legacy-data-validator-stage/src/main/java/io/mosip/registration/processor/stages/legacy/data/stage/LegacyDataValidateProcessor.java
@@ -105,8 +105,8 @@ public class LegacyDataValidateProcessor {
 			object.setInternalError(Boolean.FALSE);
 			isTransactionSuccessful = true;
 		} catch (DataMigrationException e) {
-			updateDTOsAndLogError(registrationStatusDto, RegistrationStatusCode.PROCESSING,
-					StatusUtil.DATA_MIGRATION_API_FAILED, RegistrationExceptionTypeCode.DATA_MIGRATION_EXCEPTION,
+			updateDTOsAndLogError(registrationStatusDto, RegistrationStatusCode.REJECTED,
+					StatusUtil.DATA_MIGRATION_API_FAILED, RegistrationExceptionTypeCode.PACKET_REJECTED,
 					description, PlatformErrorMessages.RPR_LEGACY_DATA_MIGRATION_API_FAILED, e);
 			attributes.put("FAILURE_REASON", "Potential mistype of NIN");
 			object.setNotificationAttributes(attributes);
