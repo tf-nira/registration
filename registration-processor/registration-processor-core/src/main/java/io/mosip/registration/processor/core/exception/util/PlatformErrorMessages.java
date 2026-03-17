68e4d4d68701918597b6e57af3c96ea0a4d0bf01
jagadeesh.t <jagathiyagu540@gmail.com>
2026-03-17 18:29:47 +0530

    upd

--- registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/exception/util/PlatformErrorMessages.java
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/exception/util/PlatformErrorMessages.java
@@ -422,7 +422,7 @@ public enum PlatformErrorMessages {
 			"Already card is present for applicant"),
 	
 	RPR_PVM_DECLARED_AS_DECEASED(PlatformConstants.RPR_PACKET_VALIDATOR_MODULE + "021",
-			"NIN is Deactivated"),
+			"Provided NIN is declared as deceased"),
 
 
 	/** The packet classification failed. */
