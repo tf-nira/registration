6a3e9e602c172076b4188e049726657d9d5076aa
malaypandey-technoforte <malay.pandey@technoforte.co.in>
2026-03-20 19:07:04 +0530

    Update MVSServiceImpl.java

--- registration-processor/core-processor/registration-processor-mvs-stage/src/main/java/io/mosip/registration/processor/mvs/service/impl/MVSServiceImpl.java
+++ registration-processor/core-processor/registration-processor-mvs-stage/src/main/java/io/mosip/registration/processor/mvs/service/impl/MVSServiceImpl.java
@@ -530,9 +530,23 @@ public class MVSServiceImpl implements MVSService {
 		requestDto.setIdentity(
 				packetManagerService.getFields(id, demographicMap.values().stream().collect(Collectors.toList()),
 						process, ProviderStageName.MVS));
+		
+		String userServiceTypeValue =null;
+		String value = null;
+		JSONArray userServiceTypeArray = null;
+		try {
+   			  userServiceTypeArray =
+           		 new JSONArray(requestDto.getIdentity().get("userServiceType"));
 
-		String userServiceTypeValue;
-		if (process.equals("RENEWAL")) {
+   			 if (userServiceTypeArray.length() > 0) {
+      		  value = userServiceTypeArray.getJSONObject(0).optString("value", null);
+   		 }
+		} catch (Exception e) {
+  					  value = null; 
+		}
+		if (value != null){
+          userServiceTypeValue = value;
+		}else if (process.equals("RENEWAL")) {
 			userServiceTypeValue = "Renewal";
 		} else if (process.equals("FIRSTID")) {
 			userServiceTypeValue = "GetFirst ID";
