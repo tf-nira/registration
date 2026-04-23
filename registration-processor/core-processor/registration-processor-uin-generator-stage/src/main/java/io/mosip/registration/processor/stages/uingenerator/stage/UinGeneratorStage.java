9f764828a933b134fe7c712bfa37018695857b49
Manishch22 <126442522+Manishch22@users.noreply.github.com>
2026-04-23 16:24:09 +0530

    Update UinGeneratorStage.java

--- registration-processor/core-processor/registration-processor-uin-generator-stage/src/main/java/io/mosip/registration/processor/stages/uingenerator/stage/UinGeneratorStage.java
+++ registration-processor/core-processor/registration-processor-uin-generator-stage/src/main/java/io/mosip/registration/processor/stages/uingenerator/stage/UinGeneratorStage.java
@@ -261,7 +261,8 @@ public class UinGeneratorStage extends MosipVerticleAPIManager {
 				        && (RegistrationType.UPDATE.toString().equalsIgnoreCase(object.getReg_type())
 				                || RegistrationType.RES_UPDATE.toString().equalsIgnoreCase(object.getReg_type())
 								|| RegistrationType.RENEWAL.toString().equalsIgnoreCase(object.getReg_type())
-								|| RegistrationType.FIRSTID.toString().equalsIgnoreCase(object.getReg_type()))) {
+								|| RegistrationType.FIRSTID.toString().equalsIgnoreCase(object.getReg_type())
+						        || RegistrationType.DEACTIVATED.toString().equalsIgnoreCase(object.getReg_type()))) {
 					String handleField = fieldMap.get(MappingJsonConstants.NIN);
 					if (StringUtils.isNotEmpty(handleField) && !handleField.equalsIgnoreCase("null")) {
 						JSONObject jsonObject = utility.getIdentityJSONObjectByHandle(handleField);
