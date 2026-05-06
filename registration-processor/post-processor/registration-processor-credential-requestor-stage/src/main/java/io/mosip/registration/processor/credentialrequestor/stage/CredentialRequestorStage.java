58a51c5b1b5a97816280c0e3c90a0f4097839224
Karthik <karthickktk2003@gmail.com>
2026-05-06 15:36:18 +0530

    alien services prn

--- registration-processor/post-processor/registration-processor-credential-requestor-stage/src/main/java/io/mosip/registration/processor/credentialrequestor/stage/CredentialRequestorStage.java
+++ registration-processor/post-processor/registration-processor-credential-requestor-stage/src/main/java/io/mosip/registration/processor/credentialrequestor/stage/CredentialRequestorStage.java
@@ -290,10 +290,6 @@ public class CredentialRequestorStage extends MosipVerticleAPIManager {
 				if (!isAdult && !isAlien) {
 					filteredPartners.removeIf(p -> "printPartner".equals(p.getId()));
 				}
-
-				if (!isAdult) {
-				    filteredPartners.removeIf(p -> "digitalcardPartner".equals(p.getId()));
-				}
 				
 				for (CredentialPartner key : filteredPartners) {
 					CredentialRequestDto credentialRequestDto = getCredentialRequestDto(regId, registrationStatusDto.getRegistrationType(), key);
