e8fe543ffe88252fae57dafff2118bcc1e002c86
Soumya Raj <soumya.raj@technoforte.co.in>
2026-05-12 15:55:40 +0530

    Enhance user service handling for deactivated aliens by refining attribute assignments and improving phone/email number processing

--- registration-processor/registration-processor-message-sender-impl/src/main/java/io/mosip/registration/processor/message/sender/service/impl/MessageNotificationServiceImpl.java
+++ registration-processor/registration-processor-message-sender-impl/src/main/java/io/mosip/registration/processor/message/sender/service/impl/MessageNotificationServiceImpl.java
@@ -233,6 +233,7 @@ public class MessageNotificationServiceImpl
 				}else if ("FIRSTID".equals(regType)) {
 					userService = "Get First ID";
 				}else if ("Alien Deactivated".equalsIgnoreCase(userServiceType)) {
+					userService = userServiceType;
 					attributes.put("IDENTITY", "Alien Identification Number (AIN)");
 				}
 				
@@ -271,7 +272,7 @@ public class MessageNotificationServiceImpl
 						regProcLogger.error("Error while extracting userServiceType", e);
 					}
 
-					smsDto.setNumber(dcicPhone.toString());
+					smsDto.setNumber(dcicPhone);
 				} else {
 					smsDto.setNumber(phoneNumber.toString());
 				}
@@ -383,6 +384,9 @@ public class MessageNotificationServiceImpl
 					userService = "Renewal Of Card";
 				}else if ("FIRSTID".equals(regType)) {
 					userService = "Get First ID";
+				} else if ("Alien Deactivated".equalsIgnoreCase(userServiceType)) {
+					userService = userServiceType;
+					attributes.put("IDENTITY", "Alien Identification Number (AIN)");
 				}
 				
 				attributes.put("service", userService);
@@ -421,7 +425,7 @@ public class MessageNotificationServiceImpl
 						regProcLogger.error("Error while extracting userServiceType", e);
 					}
 
-					mailTo = new String[]{dcicEmail.toString()};
+					mailTo = new String[]{dcicEmail};
 				}
 				
 				if("Alien New Registration".equalsIgnoreCase(userServiceType) && subject.equalsIgnoreCase("NIN Generated")) {
@@ -642,6 +646,11 @@ public class MessageNotificationServiceImpl
 		
 		if (attributes.get("surname_" + lang) == null && attributes.get("givenName_" + lang) == null) {
 			attributes.put("surname_" + lang, "applicant");
+		}
+
+		if (attributes.get("service").equals("Alien Deactivated")) {
+			attributes.put("surname_" + lang, "DCIC");
+			attributes.put("givenName_" + lang, "");
 		}
 
 		return attributes;
