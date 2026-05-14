950d5e84d337a12729852f571983493ea015c8cf
Sowmya U B <sowmya.ub@technoforte.co.in>
2026-05-14 11:18:40 +0530

    code changes

--- registration-processor/registration-processor-info-storage-service/src/main/java/io/mosip/registration/processor/packet/storage/utils/ABISHandlerUtil.java
+++ registration-processor/registration-processor-info-storage-service/src/main/java/io/mosip/registration/processor/packet/storage/utils/ABISHandlerUtil.java
@@ -103,12 +103,16 @@ public class ABISHandlerUtil {
 						List<RegistrationStatusEntity> matchedRegistrationStatusEntities = packetInfoDao
 								.getWithoutStatusCode(matchedRegIds,
 										RegistrationStatusCode.REJECTED.toString());
+						if (registrationType.equalsIgnoreCase(SyncTypeDto.NEW.toString())
+								|| registrationType.equalsIgnoreCase(SyncTypeDto.MIGRATOR.toString())
+								) {
 						List<String> processingRegIds = matchedRegistrationStatusEntities.stream()
 								.map(RegistrationStatusEntity::getRegId)
 								.collect(Collectors.toList());
+						uniqueRIDs.addAll(processingRegIds);
+					}
 						List<String> matchedProcessedRegIds = matchedRegistrationStatusEntities.stream()
 								.map(RegistrationStatusEntity::getRegId).collect(Collectors.toList());
-						uniqueRIDs.addAll(processingRegIds);
 						Set<String> processedRegIds = getUniqueRegIds(matchedProcessedRegIds, registrationId,
 								registrationType,
 								stageName);
