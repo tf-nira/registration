4b83b61d4e9b8113c97df40aa58afd9442b025ac
Soumya Raj <soumya.raj@technoforte.co.in>
2026-05-26 11:10:47 +0530

    Add surname and given name to verification request in MVSServiceImpl

--- registration-processor/core-processor/registration-processor-mvs-stage/src/main/java/io/mosip/registration/processor/mvs/service/impl/MVSServiceImpl.java
+++ registration-processor/core-processor/registration-processor-mvs-stage/src/main/java/io/mosip/registration/processor/mvs/service/impl/MVSServiceImpl.java
@@ -569,6 +569,8 @@ public class MVSServiceImpl implements MVSService {
 		requestDto.setIdentity(fields);
 
 		verReq.setSchemaVersion(requestDto.getIdentity().get("IDSchemaVersion"));
+		verReq.setSurname(surname);
+		verReq.setGivenName(givenName);
 
 		// set documents
 		JSONObject docJson = utility.getRegistrationProcessorMappingJson(MappingJsonConstants.DOCUMENT);
