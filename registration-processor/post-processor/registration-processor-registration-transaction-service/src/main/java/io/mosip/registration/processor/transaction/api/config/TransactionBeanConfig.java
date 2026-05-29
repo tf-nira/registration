9c8ba95a2e84b52fd83bfc4b9cee423d7f616fc0
jagadeesh.t <jagathiyagu540@gmail.com>
2026-05-29 14:01:52 +0530

    updated

--- registration-processor/post-processor/registration-processor-registration-transaction-service/src/main/java/io/mosip/registration/processor/transaction/api/config/TransactionBeanConfig.java
+++ registration-processor/post-processor/registration-processor-registration-transaction-service/src/main/java/io/mosip/registration/processor/transaction/api/config/TransactionBeanConfig.java
@@ -1,5 +1,9 @@
 package io.mosip.registration.processor.transaction.api.config;
 
+import io.mosip.registration.processor.core.packet.dto.Identity;
+import io.mosip.registration.processor.core.spi.packetmanager.PacketInfoManager;
+import io.mosip.registration.processor.packet.storage.dto.ApplicantInfoDto;
+import io.mosip.registration.processor.packet.storage.service.impl.PacketInfoManagerImpl;
 import io.mosip.registration.processor.status.service.AdditionalInfoRequestService;
 import io.mosip.registration.processor.status.service.impl.AdditionalInfoRequestServiceImpl;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +15,11 @@ public class TransactionBeanConfig {
     @Bean
     public AdditionalInfoRequestService additionalInfoRequestService() {
         return new AdditionalInfoRequestServiceImpl();
+    }
+
+    @Bean
+    public PacketInfoManager<Identity, ApplicantInfoDto> getPacketInfoManager() {
+        return new PacketInfoManagerImpl();
     }
 
 }
