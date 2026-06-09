008402b9273a59fef404caed6f0493517a6ec89d
jagadeesh.t <jagathiyagu540@gmail.com>
2026-06-09 14:54:38 +0530

    MA matches updated

--- registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/constant/MappingJsonConstants.java
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/constant/MappingJsonConstants.java
@@ -45,28 +45,42 @@ public class MappingJsonConstants {
 	public static final String LOCATION_HIERARCHY_FOR_PROFILING = "locationHierarchyForProfiling";
 	
 	
-/* Citizenship Verification */
-public static final String APPLICANT_TRIBE = "applicantPlaceOfOriginIndigenousCommunityTribe";
-public static final String APPLICANT_CLAN = "applicantPlaceOfOriginClan";
-public static final String FATHER_NIN = "fatherNIN";
-public static final String PARENT_TRIBE = "applicantPlaceOfOriginIndigenousCommunityTribe";
-public static final String PARENT_CLAN = "applicantPlaceOfOriginClan";
-public static final String MOTHER_NIN = "motherNIN";
-public static final String GUARDIAN_NIN = "guardianNIN_AIN";
-public static final String GUARDIAN_RELATION_TO_APPLICANT = "guardianRelationToApplicant";
-public static final String GUARDIAN_CLAN = "applicantPlaceOfOriginClan";
-public static final String GUARDIAN_TRIBE = "applicantPlaceOfOriginIndigenousCommunityTribe";
-public static final String APPLICANT_DATEOFBIRTH = "dateOfBirth";
-public static final String APPLICANT_CITIZENSHIPTYPE = "userServiceType";
-public static final String REPLACEMENT_TYPE = "userServiceTypeReplacement";
-public static final String CHANGE_IN_APPLICANT_CITIZENSHIPTYPE = "citizenshipTypeCop";
-public static final String CHANGE_APPLICANT_CITIZENSHIPTYPECOP = "changeInCitizenshipType";
-public static final String DEPENDENT_RID = "dependentRid";
-public static final String CLAN = "Clan";
-public static final String TRIBE = "Tribe";
-public static final String PERSON = "Guardian";
-public static final String DATE_FORMAT = "yyyy/MM/dd";
-public static final String NIN_STATUS = "status";
-public static final String DISABILITY = "disabilities";
-public static final String NUMBEROFOTHERSPOUSES = "numberOfOtherSpouses";
+	/* Citizenship Verification */
+	public static final String APPLICANT_TRIBE = "applicantPlaceOfOriginIndigenousCommunityTribe";
+	public static final String APPLICANT_CLAN = "applicantPlaceOfOriginClan";
+	public static final String FATHER_NIN = "fatherNIN";
+	public static final String PARENT_TRIBE = "applicantPlaceOfOriginIndigenousCommunityTribe";
+	public static final String PARENT_CLAN = "applicantPlaceOfOriginClan";
+	public static final String MOTHER_NIN = "motherNIN";
+	public static final String GUARDIAN_NIN = "guardianNIN_AIN";
+	public static final String GUARDIAN_RELATION_TO_APPLICANT = "guardianRelationToApplicant";
+	public static final String GUARDIAN_CLAN = "applicantPlaceOfOriginClan";
+	public static final String GUARDIAN_TRIBE = "applicantPlaceOfOriginIndigenousCommunityTribe";
+	public static final String APPLICANT_DATEOFBIRTH = "dateOfBirth";
+	public static final String APPLICANT_CITIZENSHIPTYPE = "userServiceType";
+	public static final String REPLACEMENT_TYPE = "userServiceTypeReplacement";
+	public static final String CHANGE_IN_APPLICANT_CITIZENSHIPTYPE = "citizenshipTypeCop";
+	public static final String CHANGE_APPLICANT_CITIZENSHIPTYPECOP = "changeInCitizenshipType";
+	public static final String DEPENDENT_RID = "dependentRid";
+	public static final String CLAN = "Clan";
+	public static final String TRIBE = "Tribe";
+	public static final String PERSON = "Guardian";
+	public static final String DATE_FORMAT = "yyyy/MM/dd";
+	public static final String NIN_STATUS = "status";
+	public static final String DISABILITY = "disabilities";
+	public static final String NUMBEROFOTHERSPOUSES = "numberOfOtherSpouses";
+
+	/* Transaction Service */
+	public static final String INQUEUE = "INQUEUE";
+	public static final String RID = "rid";
+	public static final String INTRODUCER_VALIDATION_FAILURE = "INTRODUCER_VALIDATION_FAILURE";
+	public static final String BIO_AUTH_FAILURE = "BIO_AUTH_FAILURE";
+	public static final String ANALYTICS = "analytics";
+	public static final String PRIMARY_OPERATOR_COMMENTS = "primaryOperatorComments";
+	public static final String MATCHED = "MATCHED";
+	public static final String REF_ID = "referenceId";
+	public static final String CANDIDATE_LIST = "candidateList";
+	public static final String CANDIDATES = "candidates";
+
+
 }
