fa717e48f82b3fa4dbfb1c2125354087352f0dd3
jagadeesh.t <jagathiyagu540@gmail.com>
2026-07-03 12:09:34 +0530

    notify update for cancellation

--- registration-processor/registration-processor-message-sender-impl/src/main/java/io/mosip/registration/processor/message/sender/service/impl/MessageNotificationServiceImpl.java
+++ registration-processor/registration-processor-message-sender-impl/src/main/java/io/mosip/registration/processor/message/sender/service/impl/MessageNotificationServiceImpl.java
@@ -247,16 +247,12 @@ public class MessageNotificationServiceImpl
 				}else {
 				artifact = artifact + LINE_SEPARATOR + IOUtils.toString(stream, ENCODING);;
 				}
-				smsDto.setNumber(phoneNumber.toString());
 
-//				if (process.equals("DEACTIVATED")) {
-//					String dcicPhone =  packetManagerService.getField(id, "DCICphone", regType, ProviderStageName.MESSAGE_SENDER);
-//					if (dcicPhone != null && !dcicPhone.isEmpty()) smsDto.setNumber(dcicPhone);
-//				} else if (phoneNumber != null && phoneNumber.length() != 0) {
-//					smsDto.setNumber(phoneNumber.toString());
-//				} else {
-//					throw new PhoneNumberNotFoundException(PlatformErrorMessages.RPR_SMS_PHONE_NUMBER_NOT_FOUND.getCode());
-//				}
+				if (phoneNumber != null && phoneNumber.length() != 0) {
+					smsDto.setNumber(phoneNumber.toString());
+				} else {
+					throw new PhoneNumberNotFoundException(PlatformErrorMessages.RPR_SMS_PHONE_NUMBER_NOT_FOUND.getCode());
+				}
 
 			}
 
@@ -382,16 +378,13 @@ public class MessageNotificationServiceImpl
 
 				subject=IOUtils.toString(subStream, ENCODING);
 
-				String[] mailTo = new String[]{ emailId.toString() };
+				String[] mailTo = null;
 
-//				if (process.equals("DEACTIVATED")) {
-//					String dcicEmail =  packetManagerService.getField(id, "DCICemail", regType, ProviderStageName.MESSAGE_SENDER);
-//					if (dcicEmail != null && !dcicEmail.isEmpty()) mailTo = new String[]{ dcicEmail };
-//				} else if (emailId != null && emailId.length() != 0) {
-//					mailTo = new String[]{ emailId.toString() };
-//				} else {
-//					throw new EmailIdNotFoundException(PlatformErrorMessages.RPR_EML_EMAILID_NOT_FOUND.getCode());
-//				}
+				if (emailId != null && emailId.length() != 0) {
+					mailTo = new String[]{ emailId.toString() };
+				} else {
+					throw new EmailIdNotFoundException(PlatformErrorMessages.RPR_EML_EMAILID_NOT_FOUND.getCode());
+				}
 
 				if("Alien New Registration".equalsIgnoreCase(userServiceType) && subject.equalsIgnoreCase("NIN Generated")) {
 					subject = "AIN Generated";
