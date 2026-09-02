894196d0e26acba087d475307b6bc363be59f65a
Soumya Raj <soumya.raj@technoforte.co.in>
2026-09-02 11:34:54 +0530

    remove print partner for DEACTIVATED flow

--- registration-processor/post-processor/registration-processor-credential-requestor-stage/src/main/java/io/mosip/registration/processor/credentialrequestor/stage/CredentialRequestorStage.java
+++ registration-processor/post-processor/registration-processor-credential-requestor-stage/src/main/java/io/mosip/registration/processor/credentialrequestor/stage/CredentialRequestorStage.java
@@ -288,7 +288,7 @@ public class CredentialRequestorStage extends MosipVerticleAPIManager {
 				boolean isAlien = "Alien New Registration".equals(userServiceType) || 
 								   "Renewal of Alien".equals(userServiceType);
 
-				if (!isAdult && !isAlien) {
+				if ((!isAdult && !isAlien) || "DEACTIVATED".equals(object.getReg_type())) {
 					filteredPartners.removeIf(p -> "printPartner".equals(p.getId()));
 				}
 
