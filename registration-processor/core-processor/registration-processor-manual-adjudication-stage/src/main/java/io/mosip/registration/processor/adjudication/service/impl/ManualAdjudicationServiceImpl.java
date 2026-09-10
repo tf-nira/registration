e4f89c3d081a73f2db8b3e784ac75dfa3050e128
Soumya Raj <soumya.raj@technoforte.co.in>
2026-09-10 16:59:59 +0530

    Add status only for gallery data in MA

--- registration-processor/core-processor/registration-processor-manual-adjudication-stage/src/main/java/io/mosip/registration/processor/adjudication/service/impl/ManualAdjudicationServiceImpl.java
+++ registration-processor/core-processor/registration-processor-manual-adjudication-stage/src/main/java/io/mosip/registration/processor/adjudication/service/impl/ManualAdjudicationServiceImpl.java
@@ -424,7 +424,7 @@ public class ManualAdjudicationServiceImpl implements ManualAdjudicationService 
 
 	}
 
-	private String getDataShareUrl(String id, InternalRegistrationStatusDto registrationStatusDto) throws Exception {
+	private String getDataShareUrl(String id, InternalRegistrationStatusDto registrationStatusDto, Boolean isGallery) throws Exception {
 		DataShareRequestDto requestDto = new DataShareRequestDto();
 
 		LinkedHashMap<String, Object> policy = getPolicy();
@@ -447,7 +447,7 @@ public class ManualAdjudicationServiceImpl implements ManualAdjudicationService 
 			}
 			identity.put("status", responseDTO.getStatus());
 
-		} else {
+		} else if (isGallery) {
 			identity.put("status", "IN_PROGRESS");
 		}
 
@@ -773,7 +773,7 @@ public class ManualAdjudicationServiceImpl implements ManualAdjudicationService 
 				req.setReferenceURL(getDataShareUrlForIntroducer(messageDTO.getRid(), mve.get(0).getId().getMatchedRefId(), messageDTO.getReg_type()));
 			} else {
 				req.setReferenceURL(
-						getDataShareUrl(mve.get(0).getRegId(), registrationStatusDto));
+						getDataShareUrl(mve.get(0).getRegId(), registrationStatusDto, false));
 			}
 
 		} catch (PacketManagerException | ApisResourceAccessException ex) {
@@ -801,7 +801,7 @@ public class ManualAdjudicationServiceImpl implements ManualAdjudicationService 
 								e.getId().getMatchedRefId()
 						);
 					}
-					r.setReferenceURL(getDataShareUrl(e.getId().getMatchedRefId(),registrationStatusDto1));
+					r.setReferenceURL(getDataShareUrl(e.getId().getMatchedRefId(),registrationStatusDto1, true));
 					if (r.getReferenceURL() == null || r.getReferenceURL().isBlank()) {
 						regProcLogger.info(
 								"Reference URL is null/empty. regId={}, matchedRefId={}, ReferenceId={}",
@@ -933,7 +933,7 @@ public class ManualAdjudicationServiceImpl implements ManualAdjudicationService 
 			referenceURL.setSource(PACKET);
 			referenceURL.setStatus(registrationStatusDto.getStatusCode());
 			referenceURL.setURL(
-					getDataShareUrl(id,registrationStatusDto));
+					getDataShareUrl(id,registrationStatusDto, false));
 			referenceURLs.add(referenceURL);
 			if(registrationStatusDto.getRegistrationType().equalsIgnoreCase(RegistrationType.UPDATE.name())
 					|| registrationStatusDto.getRegistrationType().equalsIgnoreCase(RegistrationType.RES_UPDATE.name())
