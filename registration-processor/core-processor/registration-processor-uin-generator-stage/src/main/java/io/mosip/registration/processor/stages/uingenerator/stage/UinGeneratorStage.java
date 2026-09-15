af0ec835062c64a5bb13de5aa6c72740f3909286
Jagadeesh-T440 <jagadeesh.thiyagarajan@technoforte.co.in>
2026-09-15 13:44:46 +0530

    Update UinGeneratorStage.java

--- registration-processor/core-processor/registration-processor-uin-generator-stage/src/main/java/io/mosip/registration/processor/stages/uingenerator/stage/UinGeneratorStage.java
+++ registration-processor/core-processor/registration-processor-uin-generator-stage/src/main/java/io/mosip/registration/processor/stages/uingenerator/stage/UinGeneratorStage.java
@@ -283,10 +283,16 @@ public class UinGeneratorStage extends MosipVerticleAPIManager {
 				}
 				
 				if(RegistrationType.DEACTIVATED.toString().equalsIgnoreCase(object.getReg_type())) {
-					demographicIdentity.put("declaredAsDeceased", "Y");
-					regProcLogger.info("Flag declaredAsDeceased added for the deactivation request reg_id: {}", registrationId);
+					if(isAlienDeactivated(demographicIdentity)) {
+						if(isDeathRegistration(demographicIdentity)) {
+							demographicIdentity.put("declaredAsDeceased", "Y");
+							regProcLogger.info("Flag declaredAsDeceased added for the death registration alien deactivation request reg_id: {}", registrationId);
+						}
+					} else {
+						demographicIdentity.put("declaredAsDeceased", "Y");
+						regProcLogger.info("Flag declaredAsDeceased added for the Non-alien deactivation request reg_id: {}", registrationId);
+					}
 				}
-
 
 				if (StringUtils.isEmpty(uinField) || uinField.equalsIgnoreCase("null") ) {
 
@@ -899,6 +905,20 @@ public class UinGeneratorStage extends MosipVerticleAPIManager {
 			}
 		} catch (Exception e) {
 			regProcLogger.error("Error while reading userServiceType from demographicIdentity", e);
+		}
+		return false;
+	}
+
+	private boolean isDeathRegistration(JSONObject demographicIdentity) {
+		try {
+			List<Map<String, String>> remarkList =
+					(List<Map<String, String>>) demographicIdentity.get(MappingJsonConstants.REMARK);
+			if (remarkList != null && !remarkList.isEmpty()) {
+				String value = remarkList.get(0).get(MappingJsonConstants.VALUE);
+				return MappingJsonConstants.DEATH.equalsIgnoreCase(value);
+			}
+		} catch (Exception e) {
+			regProcLogger.error("Error while reading remark from demographicIdentity", e);
 		}
 		return false;
 	}
